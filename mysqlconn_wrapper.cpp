4cd15d4a2fac9de44d5ff825f9ce23e773e88635
Alan Percy Chlds <alan@megazirt.co.uk>
2019-05-06 23:44:43 +0100

    Fix various memory leaks
    Delete and close memory allocations. Add a 'fin' operation to safely allow closing the database connection.

--- mysqlconn_wrapper.cpp
+++ mysqlconn_wrapper.cpp
@@ -1,109 +1,103 @@
 /* 
  * File:   mysqlconn_wrapper.h
- * Author: Eduardo Casas (www.eduardocasas.com)
  *
- * Created on February 24, 2013, 5:07 PM
+ * This is a wrapper class for the MariaDB Connector/C++ Library
+ * Developed from the wrapper by Eduardo Casas (www.eduardocasas.com & https://github.com/eduardocasas/MySQL-Connector-Cpp-Wrapper-Class)
+ * Various code added and removed (notably the memory leaking prep_stmt stuff); Alan Childs.
+ * To build probably do something like:    apt-get install libmariadb-client-lgpl-dev libmysql++-dev
  *
- * This is a wrapper class for the MySQL Connector/C++ Library
- * see: http://dev.mysql.com/doc/connector-cpp/en/index.html
  */
 
-#include "mysql_connection.h"
-	
+#include <mysql_connection.h>
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
 #include <cppconn/resultset.h>
 #include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
 
 #include "mysqlconn_wrapper.h"
 
-using namespace std;
-
 MySQLConnWrapper::~MySQLConnWrapper()
 {
-    delete res;
-    delete prep_stmt;
-    delete stmt;
-    delete con;
+	fin();
 }
 
 void MySQLConnWrapper::manageException(sql::SQLException& e)
 {
-    if (e.getErrorCode() != 0){
-        cout << "# ERR: SQLException in " << __FILE__;
-        cout << "(" << __FUNCTION__ << ") on line " << __LINE__ << endl;
-        cout << "# ERR: " << e.what();
-        cout << " (MySQL error code: " << e.getErrorCode();
-        cout << ", SQLState: " << e.getSQLState() << " )" << endl;
-    }
+	if (e.getErrorCode() != 0){
+		//You could also try and restart MySQL, or Throw and let the caller handle an error.
+		cout << "\n\033[1;31mFATAL ERROR: Connection to database failed. \n " << e.what() << "\033[0m \n";
+		exit(0); 
+	}
 }
 
 void MySQLConnWrapper::connect()
 {
-    try{
-        driver = get_driver_instance();
-        con = driver->connect(host, user, password);
-    } catch (sql::SQLException &e){
-        manageException(e);
-    }
+	try{
+		con = driver->connect(addr, user, password);
+	} catch (sql::SQLException &e){
+		manageException(e);
+	}
 }
 
 void MySQLConnWrapper::switchDb(const string& db_name)
 {
-    try{
-        con->setSchema(db_name);
-        stmt = con->createStatement();
-    } catch (sql::SQLException &e) {
-        manageException(e);
-    }
-}
-
-void MySQLConnWrapper::prepare(const string& query)
-{
-    try{
-        prep_stmt = con->prepareStatement(query);
-    } catch (sql::SQLException &e){
-        manageException(e);
-    }
-}
-
-void MySQLConnWrapper::setInt(const int& num, const int& data)
-{
-    prep_stmt->setInt(num, data);
-}
-
-void MySQLConnWrapper::setString(const int& num, const string& data)
-{
-    prep_stmt->setString(num, data);
+	try{
+		con->setSchema(db_name);
+		stmt = con->createStatement();
+	} catch (sql::SQLException &e) {
+		manageException(e);
+	}
 }
 
 void MySQLConnWrapper::execute(const string& query)
 {
+	try {
+		stmt->executeQuery(query);
+	} catch (sql::SQLException &e) {
+		manageException(e);
+	}
+}
 
-    try {
-    	if (query != "") {
-    		res = stmt->executeQuery(query);
-    	} else {
-    		res = prep_stmt->executeQuery();
-    		prep_stmt->close();
-    	}
-    } catch (sql::SQLException &e) {
-        manageException(e);
-    }
+void MySQLConnWrapper::query(const string& query)
+{
+	if (!res == 0) { delete res; res = 0;} //In case we didn't close the connect before making a new query, stops memory leak of res.
+	try {
+		res = stmt->executeQuery(query);
+	} catch (sql::SQLException &e) {
+		manageException(e);
+	}
 }
 
 bool MySQLConnWrapper::fetch()
 {
-    return res->next();
+	return res->next();
 }
 
 string MySQLConnWrapper::print(const string& field)
 {
-    return res->getString(field);
+	try {
+		return res->getString(field);
+	} catch (sql::SQLException &e) {
+        	manageException(e);
+        	return "ERROR";
+    	}
 }
 
 string MySQLConnWrapper::print(const int& index)
 {
-    return res->getString(index);
+	try {
+		return res->getString(index);
+	} catch (sql::SQLException &e) {
+		manageException(e);
+		return "ERROR";
+	}
+}
+
+void MySQLConnWrapper::fin()
+{
+	if (!res == 0) { delete res; res = 0;} //Possibly we only ran an execute and no res generating query.
+	delete stmt;
+	con -> close();
+	delete con;
+	con = NULL; //TODO: Check, but this really doesn't seem to do anything at this point. Probs needs removing.
 }
